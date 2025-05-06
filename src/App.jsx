0bde1ad6323d19cf0db847e442a22b763d68ff6f
SDivyaSravani <sankarasettydivyasravani.com>
2025-05-06 16:35:25 +0530

    Preparing for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
         <div className='app'>
             <Navbar />
 
+
             <MovieList type='popular' title='Popular' emoji={Fire} />
             <MovieList type='top_rated' title='Top Rated' emoji={Star} />
             <MovieList type='upcoming' title='Upcoming' emoji={Party} />
